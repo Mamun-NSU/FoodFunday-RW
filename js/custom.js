eecd6450453f1e89f883826dcc29f500718d8170
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2021-02-21 23:36:52 +0600

    JS
    JavaScript code

--- js/custom.js
+++ js/custom.js
@@ -179,4 +179,46 @@
 			});
 			
 			 
-		 	
+		 /* ================================================
+			COLOR PANEL OPEN/CLOSE
+		================================================ */
+		 
+		 $( "#color-panel .panel-button" ).click(function(){
+			$( "#color-panel" ).toggleClass( "close-color-panel", "open-color-panel", 1000 );
+			$( "#color-panel" ).toggleClass( "open-color-panel", "close-color-panel", 1000 );
+			return false;
+		});
+		// Color Skins
+		$('.switcher').click(function(){
+			var title = jQuery(this).attr('title');		
+			jQuery('#changeable-colors').attr('href', 'css/colors/' + title + '.css');				
+			return false;
+		});	
+		
+		jQuery(".orange-bg").on('click',function(){
+			jQuery(".logo-header img").attr("src", "images/logo.png");
+			jQuery(".footer-logo .text-center img").attr("src", "images/logo.png");
+			return false;
+		});
+
+		jQuery(".strong-blue-bg").on('click',function(){
+			jQuery(".logo-header img").attr("src", "images/logo2.png");
+			jQuery(".footer-logo .text-center img").attr("src", "images/logo2.png");
+			return false;
+		});
+
+		jQuery(".moderate-green-bg").on('click',function(){
+			jQuery(".logo-header img").attr("src", "images/logo3.png");
+			jQuery(".footer-logo .text-center img").attr("src", "images/logo3.png");
+			return false;
+		});
+
+		jQuery(".vivid-yellow-bg").on('click',function(){
+			jQuery(".logo-header img").attr("src", "images/logo4.png");
+			jQuery(".footer-logo .text-center img").attr("src", "images/logo4.png");
+			return false;
+		});
+
+
+
+	
