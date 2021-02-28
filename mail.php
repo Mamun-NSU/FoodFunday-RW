60b87a4018e5fd2b230f60e873d6b7ccf4dca81b
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2021-02-28 23:22:50 +0600

    PHP
    PHP code of gmail

--- mail.php
+++ mail.php
@@ -36,3 +36,53 @@ if(trim($date-picker) == '') {
 if(get_magic_quotes_gpc()) {
 	$comments = stripslashes($comments);
 }
+
+// Configuration option.
+// Enter the email address that you want to emails to be sent to.
+// Example $address = "joe.doe@yourdomain.com";
+
+//$address = "example@themeforest.net";
+$address = "example123789@yourdomain.com";
+
+
+// Configuration option.
+// i.e. The standard subject will appear as, "You've been contacted by John Doe."
+
+// Example, $e_subject = '$name . ' has contacted you via Your Website.';
+
+$e_subject = 'You\'ve been contacted by ' . $form_name . '.';
+
+
+// Configuration option.
+// You can change this if you feel that you need to.
+// Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
+
+$e_body = "You have been contacted by $first_name. $first_name selected service of $select_service, their additional message is as follows. Customer max budge is $select_price, for this project." . PHP_EOL . PHP_EOL;
+$e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
+$e_reply = "You can contact $first_name via email, $email or via phone $phone";
+
+$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
+
+$headers = "From: $email" . PHP_EOL;
+$headers .= "Reply-To: $email" . PHP_EOL;
+$headers .= "MIME-Version: 1.0" . PHP_EOL;
+$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
+$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+
+if(mail($address, $e_subject, $msg, $headers)) {
+
+	// Email has sent successfully, echo a success page.
+
+	echo "<fieldset>";
+	echo "<div id='success_page'>";
+	echo "<h1>Email Sent Successfully.</h1>";
+	echo "<p>Thank you <strong>$form_name</strong>, your message has been submitted to us.</p>";
+	echo "</div>";
+	echo "</fieldset>";
+
+} else {
+
+	echo 'ERROR!';
+
+}
+
